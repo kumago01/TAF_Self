fa18522c4dd7d91d4be115c0644e17e198169455
KumaGo01 <govindaraj.kumarapillai@lexisnexisrisk.com>
2022-02-15 11:43:54 +0530

    COmmiting from intellij, Added working tests from CLI with maven and testng

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -2,9 +2,11 @@ import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class DemoTest extends Base{
+
     @Test
     public void openbrowserofchoice(){
         WebDriver driver = downloaddriver(browserNames.CHROME);
+        driver.manage().window().maximize();
         driver.get("https://reedelsevier.sharepoint.com/sites/LNgcentral/Pages/Resources.aspx");
         System.out.printf(driver.getTitle());
         driver.close();
